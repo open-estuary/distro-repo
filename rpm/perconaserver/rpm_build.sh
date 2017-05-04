ee2569204844f74faf089dbe743a19c90b122fdc
sjtuhjh@hotmaill.com <sjtuhjh@hotmail.com>
2017-05-04 15:26:16 +0800

    update percona server build scripts

--- rpm/perconaserver/rpm_build.sh
+++ rpm/perconaserver/rpm_build.sh
@@ -2,12 +2,14 @@
 
 CUR_DIR=$(cd `dirname $0`; pwd)
 
-#MAJOR_VERSION="56"
-#RPM_SRC_FILE="Percona-Server-56-5.6.35-rel81.0.generic.src.rpm"
-
-MAJOR_VERSION="57"
-RPM_SRC_FILE="Percona-Server-57-5.7.17-13.1.generic.src.rpm"
-echo "Please make sure it uses 5.2.1-17 or newer gcc to compile Mysql 5.7"
+if [ ! -z "${1}" ] ; then
+    MAJOR_VERSION="56"
+    RPM_SRC_FILE="Percona-Server-56-5.6.35-rel81.0.generic.src.rpm"
+else 
+    MAJOR_VERSION="57"
+    RPM_SRC_FILE="Percona-Server-57-5.7.17-13.1.generic.src.rpm"
+    echo "Please make sure it uses 5.2.1-17 or newer gcc to compile Mysql 5.7"
+fi
 
 SRC_DIR=src${MAJOR_VERSION}
 
