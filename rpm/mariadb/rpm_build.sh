1cfd4a32d8e542ab3a054d154a6bdeb39a4acd14
sjtuhjh@hotmaill.com <sjtuhjh@hotmail.com>
2017-05-09 14:36:35 +0800

    add maraidb rpm build scripts

---
+++ rpm/mariadb/rpm_build.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+CUR_DIR=$(cd `dirname $0`; pwd)
+
+MAJOR_VERSION="10.1.21"
+RPM_SRC_FILE="mariadb-10.1.21-3.fc24.src.rpm"
+
+SRC_DIR=src
+
+if [ ! -f ${CUR_DIR}/${SRC_DIR}/${RPM_SRC_FILE} ] ; then
+    sudo wget -O ${CUR_DIR}/${SRC_DIR}/${RPM_SRC_FILE} http://dl.fedoraproject.org/pub/fedora/linux/updates/24/SRPMS/m/${RPM_SRC_FILE}
+    pushd ${CUR_DIR}/${SRC_DIR} > /dev/null
+    rpm2cpio ${RPM_SRC_FILE} | cpio -div
+    popd > /dev/null
+fi
+
+#sed -i 's/x86_64/aarch64/g' ${CUR_DIR}/${SRC_DIR}/mariadb.spec
+
+${CUR_DIR}/../../utils/rpm_build.sh  ${CUR_DIR}/${SRC_DIR} mariadb.spec arch=aarch64 --without=test
