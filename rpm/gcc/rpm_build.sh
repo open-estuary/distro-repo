de4ee0a0550777f1c4dd75512ddfeebe50ec2a62
sjtuhjh@hotmaill.com <sjtuhjh@hotmail.com>
2017-05-09 09:23:44 +0800

    update gcc rpm build script

--- rpm/gcc/rpm_build.sh
+++ rpm/gcc/rpm_build.sh
@@ -2,17 +2,21 @@
 
 CUR_DIR=$(cd `dirname $0`; pwd)
 
-VERSION="5.4.0"
+VERSION="5.4.1"
 #The DATE is also specifed in gcc.spec
 DATE="20160603"
 TAR_FILENAME="gcc-""${VERSION}""-${DATE}.tar.bz2"
-SVNREV="247088"
+#SVNREV="247088"
+SVNREV="247733"
 
 nvptx_tools_gitrev="c28050f60193b3b95a18866a96f03334e874e78f"
 nvptx_newlib_gitrev="aadc8eb0ec43b7cd0dd2dfb484bae63c8b05ef24"
 
 # Prepare GCC Source 
 if [ ! -f ${CUR_DIR}/src/${TAR_FILENAME} ] ; then
+    if [ -z "$(which svn 2>/dev/null)" ] ; then
+        sudo yum install -y svn
+    fi
     #sudo wget -O ${CUR_DIR}/src/${TAR_FILENAME} http://ftp.tsukuba.wide.ad.jp/software/gcc/releases/gcc-${VERSION}/gcc-${VERSION}.tar.bz2
     svn export svn://gcc.gnu.org/svn/gcc/branches/gcc-5-branch@${SVNREV} gcc-${VERSION}-${DATE}
     tar cf - gcc-${VERSION}-${DATE} | bzip2 -9 > ${CUR_DIR}/src/gcc-${VERSION}-${DATE}.tar.bz2
@@ -20,7 +24,7 @@ if [ ! -f ${CUR_DIR}/src/${TAR_FILENAME} ] ; then
 fi
 
 # Prepare isl lib
-${CUR_DIR}/isl_build.sh
+#${CUR_DIR}/isl_build.sh
 
 # Prepare others sources files
 if [ ! -f ${CUR_DIR}/src/nvptx-tools-${nvptx_tools_gitrev}.tar.bz2 ] ; then
