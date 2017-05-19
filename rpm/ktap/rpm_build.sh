a461e00772d532a0c7c4d949e0df8d3dff7f964d
y00369814 <yulijie@huawei.com>
2017-05-19 08:51:05 +0000

    [Changelog]: add Package ktap [Author]: y00369814 [Date]:2017/5/19

---
+++ rpm/ktap/rpm_build.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+CUR_DIR=$(cd `dirname $0`; pwd)
+
+VERSION="0.4"
+TAR_FILENAME="ktap-""${VERSION}"".tar.bz2"
+
+if [ ! -f ${CUR_DIR}/src/${TAR_FILENAME} ] ; then
+	cd ${CUR_DIR}/src
+	git clone https://github.com/ktap/ktap.git
+	mv ktap ktap-${VERSION}
+	tar -jcvf ktap-${VERSION}.tar.bz2 ktap-${VERSION}
+	rm -rf ktap-${VERSION}
+	cd -
+fi
+sed -i "s/Version\:\ .*/Version\:\ \ \ ${VERSION}/g" ${CUR_DIR}/src/ktap.spec
+
+${CUR_DIR}/../../utils/rpm_autobuild.sh  ${CUR_DIR}/src ktap.spec
+
