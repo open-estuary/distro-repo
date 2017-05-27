9f2efa6b120be2589cd7c1c02b3e14ce0006623d
y00369814 <yulijie@huawei.com>
2017-05-27 16:29:41 +0800

    [Changelog]: set timeout -1 in rpm_build.sh [Author]: y00369814 [Date]: 2017/5/27

--- utils/rpm_build.sh
+++ utils/rpm_build.sh
@@ -27,7 +27,7 @@ fi
 #sudo yum-builddep -y ${SRC_DIR}/${SPEC_FILE}
 passphrase=`cat /home/KEY_PASSPHRASE`
 expect <<-END
-	set timeout 10
+	set timeout -1
 	spawn rpmbuild --sign  --target aarch64 -ba ${SRC_DIR}/${SPEC_FILE} "--define=_sourcedir ${SRC_DIR}" "--define=_specdir ${SRC_DIR}" ${@:3}
 	expect {
                 "Enter pass phrase:" {send "${passphrase}\r"}
