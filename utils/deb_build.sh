f9c2e64fb6a32b4d8571940a80b22a59f8ee92ca
y00369814 <yulijie@huawei.com>
2017-06-13 11:58:45 +0800

    [Changelog]: clean container in deb_build.sh [Author]: y00369814 [Date]: 2017/6/13

--- utils/deb_build.sh
+++ utils/deb_build.sh
@@ -26,18 +26,27 @@ SRC_DIR_3=${SRC_DIR_2#*/}
 SRC_DIR_4=${SRC_DIR_3#*/}
 TAR_FILENAME=$2
 DISTRI=$3
+Container_Name=${TAR_FILE_NAME%-*}-$DISTRI
 
 if [ $DISTRI = "debian" ]; then
-	docker run -d -v ~/:/root/ openestuary/debian:latest sh /root/distro-repo/utils/build_incontainer.sh /root/${SRC_DIR_4} ${TAR_FILENAME}
+	docker run -d -v ~/:/root/ --name ${Container_Name} openestuary/debian:latest sh /root/distro-repo/utils/build_incontainer.sh /root/${SRC_DIR_4} ${TAR_FILENAME}
 fi
 
 if [ $DISTRI = "ubuntu" ]; then
-	docker run -d -v ~/:/root/ openestuary/ubuntu:latest sh /root/distro-repo/utils/build_incontainer.sh /root/${SRC_DIR_4} ${TAR_FILENAME}
+	docker run -d -v ~/:/root/ --name ${Container_Name} openestuary/ubuntu:latest sh /root/distro-repo/utils/build_incontainer.sh /root/${SRC_DIR_4} ${TAR_FILENAME}
 fi
 
 if [ $? -ne 0 ]; then
         echo "Build failed, please check!"
 else
         echo "Build succeed."
+fi
+
+echo "Begin to remove building container."
+docker rm ${Container_Name}
+if [ $? -ne 0 ]; then
+        echo "Remove building container failed!"
+else
+        echo "Building container have been removed successfully!"
 fi
 
