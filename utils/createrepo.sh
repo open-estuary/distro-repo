210d16791dc66c26a970e9ffb4c6ec4ad981a99f
y00369814 <yulijie@huawei.com>
2017-06-19 11:55:40 +0800

    [Changlog]: add scripts createrepo.sh [Author]: y00369814 [Date]: 2017/6/19

---
+++ utils/createrepo.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+CUR_DIR=$(cd `dirname $0`; pwd)
+
+. ${CUR_DIR}/function_cmd_scp.sh
+IP="117.78.41.188"
+loginuser="repo"
+loginpassword="repodeploy@123"
+
+TARGETOS=$1
+if [ "${TARGETOS}" = "CentOS" ] || [ "${TARGETOS}" = "centos" ] ; then
+	sshcmd "createrepo /est-repo/releases/5.0/centos"	
+elif [ "${TARGETOS}" = "Ubuntu" ] || [ "${TARGETOS}" = "ubuntu" ] ; then
+	sshcmd "dpkg-scanpackages /est-repo/releases/5.0/ubuntu /dev/null | gzip > /est-repo/releases/5.0/ubuntu/Packages.gz;dpkg-scansources /est-repo/releases/5.0/ubuntu | gzip > /est-repo/releases/5.0/ubuntu/Sources.gz"
+elif [ "${TARGETOS}" = "Debian" ] || [ "${TARGETOS}" = "debian" ] ; then
+	sshcmd "dpkg-scanpackages /est-repo/releases/5.0/debian /dev/null | gzip > /est-repo/releases/5.0/debian/Packages.gz;dpkg-scansources /est-repo/releases/5.0/debian | gzip > /est-repo/releases/5.0/debian/Sources.gz"
+else
+    echo "Currently it only support CentOS, Ubuntu or Debian"
+    exit 0
+fi
+
+if [ $? -ne 0 ];then
+	echo "repo create failed!"
+else
+	echo "repo create successfully!"
+fi
