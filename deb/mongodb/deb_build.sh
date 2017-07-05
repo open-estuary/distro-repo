6d76fb62d3729636a838d095f1b67e95c83dda97
sjtuhjh@hotmaill.com <sjtuhjh@hotmail.com>
2017-07-05 16:58:31 +0800

    add mongodb deb packages

---
+++ deb/mongodb/deb_build.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+TARGET="ubuntu"
+OS_VERSION="16.04"
+
+if [ x"${1}" == x"debian" ] ; then
+    TARGET=$1
+    OS_VERSION="8"
+elif [ ! -z "${1}" ] && [ x"${1}" != x"ubuntu" ] ; then
+    echo "Unsupport platform: ${1}, it must be debian or ubuntu"
+    exit 1
+fi
+
+CUR_DIR=$(cd `dirname $0`; pwd)
+
+MAJOR="3.4"
+VERSION="${MAJOR}.6"
+SUFFIX="-rc0"
+TAR_FILENAME="mongodb-${VERSION}${SUFFIX}.tar.gz"
+SRC=src-${TARGET}
+
+if [ ! -d "${SRC}" ] ; then
+    mkdir -p ${SRC}
+fi
+
+NEW_TAR_FILENAME="mongodb_${VERSION}.tar.gz"
+if [ ! -f ${CUR_DIR}/${SRC}/${NEW_TAR_FILENAME} ] ; then
+    if [ ! -f ${CUR_DIR}/${SRC}/${TAR_FILENAME} ] ; then
+        wget -O ${CUR_DIR}/${SRC}/${TAR_FILENAME}   https://github.com/mongodb/mongo/archive/r${VERSION}${SUFFIX}.tar.gz
+    fi
+
+    pushd ${CUR_DIR}/${SRC} > /dev/null
+    tar -zxvf ${TAR_FILENAME}
+    mv mongo-r${VERSION}${SUFFIX} mongodb-${VERSION}
+    tar -zcvf ${NEW_TAR_FILENAME} mongodb-${VERSION}
+    rm -r mongo-r${VERSION}${SUFFIX}
+    rm -r mongodb-${VERSION}
+    rm ${TAR_FILENAME}
+    popd > /dev/null
+fi
+
+${CUR_DIR}/../../utils/deb_build.sh  ${CUR_DIR}/${SRC} ${NEW_TAR_FILENAME} ${TARGET}
