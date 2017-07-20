f5285c04d4812a59d8c46fc807dcbbbd50d6436d
y00369814 <yulijie@huawei.com>
2017-07-20 17:43:30 +0800

    [Changelog]: update deb_build.sh [Author]:y00369814 [Date]:2017/7/20

--- deb/ktap/deb_build.sh
+++ deb/ktap/deb_build.sh
@@ -2,26 +2,19 @@
 
 CUR_DIR=$(cd `dirname $0`; pwd)
 
-usage()
-{
-        echo "Usage: deb_build.sh [targetos(debian or ubuntu)]"
-}
-
-TARGETOS=debian
-if [ $# -eq 0 ];then
-        TARGETOS=debian
-elif [ $# -eq 1 ];then
-        TARGETOS=$1
-else
-        usage
-        exit 1
+TARGETOS="ubuntu"
+if [ x"${1}" == x"debian" ] ; then
+    TARGETOS=$1
+elif [ ! -z "${1}" ] && [ x"${1}" != x"ubuntu" ] ; then
+    echo "Unsupport platform: ${1}, it must be debian or ubuntu"
+    exit 1
 fi
 
 VERSION="0.4"
 TAR_FILENAME="ktap-""${VERSION}+git20150927"".tar.gz"
 
 if [ ! -f ${CUR_DIR}/src/${TAR_FILENAME} ] ; then
-        sudo wget -O ${CUR_DIR}/src/${TAR_FILENAME} https://github.com/ktap/ktap.git 
+        wget -O ${CUR_DIR}/src/${TAR_FILENAME} https://github.com/ktap/ktap.git 
 fi
 
 ${CUR_DIR}/../../utils/deb_build.sh  ${CUR_DIR}/src ${TAR_FILENAME} $TARGETOS
