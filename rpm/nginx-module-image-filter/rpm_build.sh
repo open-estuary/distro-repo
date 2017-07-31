bfd910f8cc32092d6af55823dfc40ba5ae97bb71
sjtuhjh@hotmaill.com <sjtuhjh@hotmail.com>
2017-07-31 16:35:17 +0800

    add nginx latest packages

---
+++ rpm/nginx-module-image-filter/rpm_build.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+TARGET_OS="centos"
+if [ ! -z "${1}" ] ; then
+    TARGET_OS=${1}
+fi
+
+CUR_DIR=$(cd `dirname $0`; pwd)
+
+VERSION="1.13.3"
+RPM_SRC_FILE="nginx-module-image-filter-1.13.3-1.el7.ngx.src.rpm"
+
+SRC_DIR=src
+export GOPATH=${CUR_DIR}/${SRC_DIR}
+
+if [ ! -f ${CUR_DIR}/${SRC_DIR}/${RPM_SRC_FILE} ] ; then
+    if [ ! -d ${CUR_DIR}/${SRC_DIR} ] ; then
+        mkdir -p ${CUR_DIR}/${SRC_DIR}
+    fi 
+    wget -O ${CUR_DIR}/${SRC_DIR}/${RPM_SRC_FILE} https://nginx.org/packages/mainline/centos/7/SRPMS/${RPM_SRC_FILE}
+
+    pushd ${CUR_DIR}/${SRC_DIR} > /dev/null
+    rpm2cpio ${RPM_SRC_FILE} | cpio -div
+    popd > /dev/null
+fi
+
+${CUR_DIR}/../../utils/rpm_build.sh  ${CUR_DIR}/${SRC_DIR} nginx-module-image-filter.spec
