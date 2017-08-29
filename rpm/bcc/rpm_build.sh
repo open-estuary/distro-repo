5e253ba60205825a34f54b186285091c36b24ce7
y00369814 <yulijie@huawei.com>
2017-08-29 17:16:51 +0800

    [Changelog]: add bcc rpm package [Author]: y00369814 [Date]: 2017/8/29

---
+++ rpm/bcc/rpm_build.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+CUR_DIR=$(cd `dirname $0`; pwd)
+
+VERSION="0.3"
+TAR_FILENAME="bcc-""${VERSION}"".tar.gz"
+
+if [ ! -f ${CUR_DIR}/src/${TAR_FILENAME} ] ; then
+	cd ${CUR_DIR}/src
+        git clone https://github.com/iovisor/bcc.git
+        mv bcc bcc-${VERSION}
+        tar -zcvf bcc-${VERSION}.tar.gz bcc-${VERSION}
+        rm -rf bcc-${VERSION}
+        cd -
+
+fi
+sed -i "s/Version\:\ .*/Version\:\ \ \ ${VERSION}/g" ${CUR_DIR}/src/bcc.spec
+
+${CUR_DIR}/../../utils/rpm_build.sh  ${CUR_DIR}/src bcc.spec
+
