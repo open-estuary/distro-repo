76470fd711379814761ad36664a1f43876db8256
y00369814 <yulijie@huawei.com>
2017-11-24 15:48:21 +0800

    [Changelog]: update python-lit [Author]: y00369814 [Date]: 2017/11/24

--- rpm/python-lit/rpm_build.sh
+++ rpm/python-lit/rpm_build.sh
@@ -8,17 +8,17 @@ CUR_DIR=$(cd `dirname $0`; pwd)
 
 #source /opt/rh/devtoolset-4/enable
 
-RPM_SRC_FILE="clang-5.0.0-2.fc28.src.rpm"
+RPM_SRC_FILE="python-lit-0.5.1-1.fc28.src.rpm"
 SRC_DIR=src
 
 if [ ! -f ${CUR_DIR}/${SRC_DIR}/${RPM_SRC_FILE} ] ; then
-    wget -O ${CUR_DIR}/${SRC_DIR}/${RPM_SRC_FILE} http://dl.fedoraproject.org/pub/fedora/linux/development/rawhide/Everything/source/tree/Packages/c/${RPM_SRC_FILE}
+    wget -O ${CUR_DIR}/${SRC_DIR}/${RPM_SRC_FILE} http://dl.fedoraproject.org/pub/fedora/linux/development/rawhide/Everything/source/tree/Packages/p/${RPM_SRC_FILE}
     pushd ${CUR_DIR}/${SRC_DIR} > /dev/null
     rpm2cpio ${RPM_SRC_FILE} | cpio -div
     popd > /dev/null
 fi
 
-mv python-lit_real.spec python-lit.spec
+mv src/python-lit_real.spec src/python-lit.spec
 SPECFILE="python-lit.spec"
 
 ${CUR_DIR}/../../utils/rpm_build.sh  ${CUR_DIR}/${SRC_DIR} ${SPECFILE} ${BUILD_DEVTOOL} 
