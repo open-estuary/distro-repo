8072422c90f0c6a386ae29fed0afc2bfb557755b
y00369814 <yulijie@huawei.com>
2017-11-27 15:21:43 +0800

    [Changelog]: update compile-rt [Author]: y00369814 [Date]:2017/11/27

--- rpm/compiler-rt/rpm_build.sh
+++ rpm/compiler-rt/rpm_build.sh
@@ -8,8 +8,8 @@ CUR_DIR=$(cd `dirname $0`; pwd)
 
 #source /opt/rh/devtoolset-4/enable
 
-VERSION="5.0.0"
-RPM_SRC_FILE="compiler-rt-5.0.0-1.fc28.src.rpm"
+VERSION="4.0.1"
+RPM_SRC_FILE="compiler-rt-4.0.1-2.fc27.src.rpm"
 SRC_DIR=src
 
 if [ ! -f ${CUR_DIR}/${SRC_DIR}/${RPM_SRC_FILE} ] ; then
@@ -22,10 +22,7 @@ fi
 #sed -i 's/x86_64/aarch64/g' ${CUR_DIR}/${SRC_DIR}/glibc.spec
 
 
-if [ ${BUILD_DEVTOOL} -eq 1 ] ; then
-    mv compiler-rt_scl.spec compiler-rt.spec
-fi
-SPECFILE="clang.spec"
+SPECFILE="compiler-rt.spec"
 
 ${CUR_DIR}/../../utils/rpm_build.sh  ${CUR_DIR}/${SRC_DIR} ${SPECFILE} 
 
