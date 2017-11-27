25f0acdd58e52cc1c08126e27c20488399512b74
y00369814 <yulijie@huawei.com>
2017-11-27 14:09:55 +0800

    [Changelog]: update llvm [Author]: y00369814 [Date]: 2017/11/27

--- rpm/llvm/rpm_build.sh
+++ rpm/llvm/rpm_build.sh
@@ -7,23 +7,19 @@ fi
 
 CUR_DIR=$(cd `dirname $0`; pwd)
 
-RPM_SRC_FILE="llvm-5.0.0-5.fc28.src.rpm"
+RPM_SRC_FILE="llvm-4.0.1-1.fc27.src.rpm"
 
 SRC_DIR=src
 
-if [ ! -f ${CUR_DIR}/${SRC_DIR}/${RPM_SRC_FILE} ] ; then
-    if [ ! -d ${CUR_DIR}/${SRC_DIR} ] ; then
-        mkdir -p ${CUR_DIR}/${SRC_DIR}
-    fi 
-    wget -O ${CUR_DIR}/${SRC_DIR}/${RPM_SRC_FILE} http://dl.fedoraproject.org/pub/fedora/linux/development/rawhide/Everything/source/tree/Packages/l/${RPM_SRC_FILE}
-    pushd ${CUR_DIR}/${SRC_DIR} > /dev/null
-    rpm2cpio ${RPM_SRC_FILE} | cpio -div
-    popd > /dev/null
-fi
-
-if [ ${BUILD_DEVTOOL} -eq 1 ] ; then
-    mv llvm_scl.spec llvm.spec
-fi
+#if [ ! -f ${CUR_DIR}/${SRC_DIR}/${RPM_SRC_FILE} ] ; then
+#    if [ ! -d ${CUR_DIR}/${SRC_DIR} ] ; then
+#        mkdir -p ${CUR_DIR}/${SRC_DIR}
+#    fi 
+#    wget -O ${CUR_DIR}/${SRC_DIR}/${RPM_SRC_FILE} http://dl.fedoraproject.org/pub/fedora/linux/development/rawhide/Everything/source/tree/Packages/l/${RPM_SRC_FILE}
+#    pushd ${CUR_DIR}/${SRC_DIR} > /dev/null
+#    rpm2cpio ${RPM_SRC_FILE} | cpio -div
+#    popd > /dev/null
+#fi
 
 
 ${CUR_DIR}/../../utils/rpm_build.sh  ${CUR_DIR}/${SRC_DIR} llvm.spec 
