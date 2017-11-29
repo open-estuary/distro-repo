fd4d6dbed795d5b04177dd19fecd6c723a1bfa6d
y00369814 <yulijie@huawei.com>
2017-11-29 15:25:59 +0800

    [Changelog]: update clang [Author]: y00369814 [Date]: 2017/11/29

--- rpm/clang/rpm_build.sh
+++ rpm/clang/rpm_build.sh
@@ -8,24 +8,27 @@ CUR_DIR=$(cd `dirname $0`; pwd)
 
 #source /opt/rh/devtoolset-4/enable
 
-VERSION="5.0.0"
-RPM_SRC_FILE="clang-5.0.0-2.fc28.src.rpm"
+VERSION="4.0.1"
+RPM_SRC_FILE="clang-4.0.1-1.fc27.src.rpm"
 SRC_DIR=src
 
-if [ ! -f ${CUR_DIR}/${SRC_DIR}/${RPM_SRC_FILE} ] ; then
-    wget -O ${CUR_DIR}/${SRC_DIR}/${RPM_SRC_FILE} http://dl.fedoraproject.org/pub/fedora/linux/development/rawhide/Everything/source/tree/Packages/c/${RPM_SRC_FILE}
-    pushd ${CUR_DIR}/${SRC_DIR} > /dev/null
-    rpm2cpio ${RPM_SRC_FILE} | cpio -div
-    popd > /dev/null
-fi
+#if [ ! -f ${CUR_DIR}/${SRC_DIR}/${RPM_SRC_FILE} ] ; then
+#    wget -O ${CUR_DIR}/${SRC_DIR}/${RPM_SRC_FILE} http://dl.fedoraproject.org/pub/fedora/linux/development/rawhide/Everything/source/tree/Packages/c/${RPM_SRC_FILE}
+#    pushd ${CUR_DIR}/${SRC_DIR} > /dev/null
+#    rpm2cpio ${RPM_SRC_FILE} | cpio -div
+#    popd > /dev/null
+#fi
 
 #sed -i 's/x86_64/aarch64/g' ${CUR_DIR}/${SRC_DIR}/glibc.spec
 
-
-if [ ${BUILD_DEVTOOL} -eq 1 ] ; then
-    mv clang_scl.spec clang.spec
-fi
+pushd ${CUR_DIR}/${SRC_DIR} > /dev/null
+cat clang-4.0.1-1.fc27.src.rpm.tar.gz.* | tar -zxv
+rpm2cpio ${RPM_SRC_FILE} | cpio -div
+popd > /dev/null
+#if [ ${BUILD_DEVTOOL} -eq 1 ] ; then
+#    mv clang_scl.spec clang.spec
+#fi
 SPECFILE="clang.spec"
 
-${CUR_DIR}/../../utils/rpm_build.sh  ${CUR_DIR}/${SRC_DIR} ${SPECFILE}  
+#${CUR_DIR}/../../utils/rpm_build.sh  ${CUR_DIR}/${SRC_DIR} ${SPECFILE}  
 
